6fa0dcfdfa1764b6108d569da4f8e0146aed4b14
Arkady Zhukov <arkady.e1ppa@gmail.com>
2023-07-16 20:06:38 +0300

    RIP ProducerFor

--- weave/futures/model/evaluation.hpp
+++ weave/futures/model/evaluation.hpp
@@ -15,12 +15,6 @@ concept Evaluation =
   eval.Start();
 };
 
-template <typename F, typename C>
-concept ProducerFor = Thunk<F> && Consumer<C, typename F::ValueType> &&
-    requires(F thunk, C& consumer) {
-  std::move(thunk).Force(consumer)->Evaluation<F, C>;
-};
-
 template <typename C, typename F> // ProducerFor<F, C> == true
 using EvaluationType = decltype(std::declval<std::add_rvalue_reference_t<F>>().Force(std::declval<std::add_lvalue_reference_t<C>>()));
 
