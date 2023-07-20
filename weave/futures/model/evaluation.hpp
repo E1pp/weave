b8b9799693de097a845de15278bf27350ef89e30
Arkady Zhukov <arkady.e1ppa@gmail.com>
2023-07-20 13:53:19 +0300

    Welcome back, ProducerFor<F, C>

--- weave/futures/model/evaluation.hpp
+++ weave/futures/model/evaluation.hpp
@@ -10,11 +10,17 @@ namespace weave::futures {
 template <typename E, typename F, typename C>
 concept Evaluation =
     !std::movable<E> && !std::copyable<E> && requires(E eval, F fut, C& cons) {
-  // E(std::move(fut), cons);
   eval.Start();
 };
 
-template <typename C, typename F>  // ProducerFor<F, C> == true
+template <typename F, typename C>
+concept ProducerFor = Thunk<F> && requires (F f, C& cons){
+  {std::move(f).Force(cons)} -> Evaluation<F, C>;
+};
+
+// Technically ProducerFor is tautalogical here
+// since Force checks Consumer<F::ValueType> anyway
+template <typename C, ProducerFor<C> F>  
 using EvaluationType =
     decltype(std::declval<std::add_rvalue_reference_t<F>>().Force(
         std::declval<std::add_lvalue_reference_t<C>>()));
